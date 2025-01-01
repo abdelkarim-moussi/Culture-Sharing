bdce7384c0dec93b092489de8b011725d5120ebd
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-01-01 18:28:10 +0100

    création de la fonction createArticle pour ajouter un article a la base de données

--- classes/Author.php
+++ classes/Author.php
@@ -1,7 +1,23 @@
 <?php
-
+session_start();
+include_once "Articles.php";
 class Author extends Visitor{
 
-    private function CreateArticle(){
+    protected function CreateArticle($author_id,$categorie_id,$title,$content,$image,$date_pub){
+
+        if(empty($title) || empty($content) || empty($image) || empty($categorie)){
+            header("Location: ../public/authorDash.php?error=emptyfields");
+            exit();
+        }
+
+        $stmt = $this->connect()->prepare("INSERT INTO articles(user_id,categorie_id,title,content,pub_date,image) VALUES(?,?,?,?,?,?);");
+
+        if(!$stmt->execute([$author_id,$categorie_id,$title,$content,$date_pub,$image])){
+            $stmt = null;
+            header("Location: ../public/authorDash.php?error=executionfailed");
+            exit();
+        }
+
+
     }
 }

62e552dc8e4d8cf4204baea51580578251394db8
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-01-01 20:23:01 +0100

    create the class for the author and the method to ceate an article

--- classes/Author.php
+++ classes/Author.php
@@ -3,13 +3,16 @@ session_start();
 include_once "Articles.php";
 class Author extends Visitor{
 
-    protected function CreateArticle($author_id,$categorie_id,$title,$content,$image,$date_pub){
+    protected function CreateArticle($title,$categorie,$content,$image){
 
-        if(empty($title) || empty($content) || empty($image) || empty($categorie)){
-            header("Location: ../public/authorDash.php?error=emptyfields");
-            exit();
-        }
+        $author_id = $_SESSION["userId"];
+        $cat = $this->connect()->prepare("SELECT categorie_id categories WHERE categorie_name = $categorie");
+        $cat ->execute();
+        $cat->fetchALL();
+        $categorie_id = $cat[0]["categorie_id"];
 
+        $date_pub = date("y-m-d");
+        
         $stmt = $this->connect()->prepare("INSERT INTO articles(user_id,categorie_id,title,content,pub_date,image) VALUES(?,?,?,?,?,?);");
 
         if(!$stmt->execute([$author_id,$categorie_id,$title,$content,$date_pub,$image])){
